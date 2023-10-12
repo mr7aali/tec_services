830b412769ecad7a0543b95743454c6147c6bd3c
mr7aali <mr7aali@gmail.com>
2023-10-12 21:08:06 +0600

    added google font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { Roboto_Slab} from "next/font/google";
+
+const roboto = Roboto_Slab({
+  weight: ["400","800"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
