faf73cc3d4e72414efe1324fe4dff9148e16a8da
mr7aali <mr7aali@gmail.com>
2023-10-13 10:28:16 +0600

    header top done

--- src/components/sheared/Header.tsx
+++ src/components/sheared/Header.tsx
@@ -1,15 +1,45 @@
+import { UserAddOutlined } from "@ant-design/icons";
+
 const HeaderCom = () => {
   return (
-    <div className="container mx-auto">
+    <>
       <HeaderTop />
-      <h1>This is nav bar</h1>
-    </div>
+      <div></div>
+    </>
   );
 };
-
 
 export default HeaderCom;
 
 const HeaderTop = () => {
-  return <div>header to[</div>;
+  return (
+    <div className="bg-[#f5f6f8]">
+      <div className="max-w-[1360px] mx-auto flex items-center py-1 ">
+        <div>
+          <h5 className="text-[11px] line-clamp-2 uppercase">
+            Welcome to Othoba .Com
+          </h5>
+        </div>
+
+        <div className="flex justify-between items-center ml-auto">
+          <h5 className="line-clamp-2 text-[11px]">Sell On Othoba</h5>
+          <span className="mx-5 w-[1px] h-[20px] bg-[#ccc]"></span>
+          <div className="flex justify-between items-center ">
+            <p className="m-2 ">
+              <span>
+                {" "}
+                <UserAddOutlined />
+              </span>
+
+              <span className="p-2 text-[11px] text-[#000] font-bold">
+                Sign In
+              </span>
+              <span className="text-[11px] font-bold mx-2">/</span>
+              <span className="text-[11px] font-bold"> Register</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
