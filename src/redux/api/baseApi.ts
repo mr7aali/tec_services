eb3fe53b3ef1f8109006610181d9f5df138209d5
mr7aali <mr7aali@gmail.com>
2023-10-20 03:00:10 +0600

    a

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
     reducerPath: 'api',
-    baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api/v1" }),
+    baseQuery: axiosBaseQuery({ baseUrl: "https://tec-services-backend.vercel.app/api/v1" }),
     endpoints: (builder) => ({
 
     }),
